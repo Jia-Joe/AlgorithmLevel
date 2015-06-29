43d7dc1407dabcb07d88ae1550912be910705a8f
Jia-Joe <472167879@qq.com>
2015-06-29 20:55:55 +0800

    Print RBtree

--- RB-tree/RB-tree/printTree.cpp
+++ RB-tree/RB-tree/printTree.cpp
@@ -5,14 +5,19 @@
 #include <math.h>
 #include <assert.h>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
-int setwN=1;
+int setwN=3;
 
-void outchar(char a, int n);
-void Routchar(char a, int n);
+void outchar(char a, int n);  //输出不带括号的字符代表黑色节点
+void outchar(char a, int n, bool isRchildBefore);
+void outchar(string a, int n);
+void Routchar(char a, int n,bool isRchildBefore); //输出含有括号的字符代表红色节点
+void Routchar(string a, int n, bool isRchildBefore);
 
+//setwN=1，输出普通二叉树
 void print2tree(vector <char> s, int n) 
 {
 	int dpt = log((double)n) / log(2.0) + 1;
@@ -23,27 +28,30 @@ void print2tree(vector <char> s, int n)
 		cout << setw(setwN)<<' ';
 	}
 	int cnt = 0;
-	cout << s[cnt++]<<endl;
+	cout << setw(setwN)<<s[cnt++] << endl;
 	for (int i = dpt-1; i>0; i--)
 	{
 		int leafmin = pow(2, i-1);//1
 		int leafstep = leafmin * 2;//2
 		int leafnum = pow(2, dpt - i);//8
 		vector<int> leafindex, leafindexbefore;
-		//输出第一行
-		outchar('|', leafstep);
+		////输出第一行
+		outchar(':', leafstep);
 		for (int ii = 1; ii < leafnum/2; ii++)
 		{
-			outchar('|', leafstep*2);
+			outchar(':', leafstep * 2);
 		}
 		cout << endl;
 		//输出第二行
 		for (int j = 0; j < leafnum/2; j++)
 		{
-			outchar('-', leafmin);
-			for (int k = 0; k < leafstep; k++)
+			outchar('.', leafmin);
+			for (int k = 0; k < setwN*leafstep; k++)
 			{
-				cout << setw(setwN)<<'-';
+				if (k == setwN*leafstep - 1)
+					cout << '.';
+				else
+					cout << ':';
 			}
 			for (int k = 0; k < leafstep/2; k++)
 			{
@@ -52,10 +60,10 @@ void print2tree(vector <char> s, int n)
 		}
 		cout << endl;
 		//输出第三行
-		outchar('|', leafmin);
+		outchar(':', leafmin);
 		for (int ii = 1; ii < leafnum; ii++)
 		{
-			outchar('|', leafstep);
+			outchar(':', leafstep);
 		}
 		cout << endl;
 		//输出第四行
@@ -68,30 +76,109 @@ void print2tree(vector <char> s, int n)
 	}
 	
 }
+//setwN>1，输出红黑树
+void printRBtree(vector <char> s, int n)
+{
+	int dpt = log((double)n) / log(2.0) + 1;
+	int mid = pow(2, dpt - 1);
+	//输出根节点,根节点一定是黑色的
+	int cnt = 0;
+	outchar(s[cnt++], mid, false);
+	bool isRchild = false;
 
-//void recurPrint(int mid,int n)
-//{
-//	if(n != 0)
-//	{
-//		int midleft = mid;
-//		int midright = mid;
-//		int dp = pow(2, n - 1);
-//		for (int i = 0; i < dp; i++)
-//		{
-//			midleft--;
-//			midright++;
-//			outchar('/', midleft);
-//			outchar('\\', midright - midleft);
-//			cout << endl;
-//		}
-//
-//		outchar('A', midleft);
-//		outchar('B', midright - midleft);
-//		cout << endl;
-//		recurPrint(midleft, n - 1);
-//		recurPrint(midright, n - 1);
-//	}
-//}
+	cout << endl;
+	for (int i = dpt - 1; i>0; i--)
+	{
+		int leafmin = pow(2, i - 1);//1
+		int leafstep = leafmin * 2;//2
+		int leafnum = pow(2, dpt - i);//8
+		vector<int> leafindex, leafindexbefore;
+		////输出第一行
+		outchar(':', leafstep);
+		for (int ii = 1; ii < leafnum / 2; ii++)
+		{
+			outchar(':', leafstep * 2);
+		}
+		cout << endl;
+		//输出第二行
+		for (int j = 0; j < leafnum / 2; j++)
+		{
+			outchar('.', leafmin);
+			for (int k = 0; k < setwN*leafstep; k++)
+			{
+				if (k == setwN*leafstep - 1)
+					cout << '.';
+				else
+					cout << ':';
+			}
+			for (int k = 0; k < leafstep / 2; k++)
+			{
+				cout << setw(setwN) << ' ';
+			}
+		}
+		cout << endl;
+		//输出第三行
+		outchar(':', leafmin);
+		for (int ii = 1; ii < leafnum; ii++)
+		{
+			outchar(':', leafstep);
+		}
+		cout << endl;
+		//输出第四行
+		if (cnt % 2== 0)
+		{
+			Routchar(s[cnt++], leafmin, false);//行首节点可以无视isChildBefore
+			isRchild = true;
+		}
+		else
+		{
+			outchar(s[cnt++], leafmin, false);
+			isRchild = false;
+		}
+		for (int ii = 1; ii < leafnum; ii++)
+		{
+
+			if (cnt % 2 == 0)
+			{
+
+				Routchar(s[cnt++], leafstep, isRchild);
+				isRchild = true;
+			}				
+			else
+			{
+				outchar(s[cnt++], leafstep, isRchild);
+				isRchild = false;
+			}
+		}
+		cout << endl;
+	}
+
+}
+
+
+void recurPrint(int mid,int n)
+{
+	if(n != 0)
+	{
+		int midleft = mid;
+		int midright = mid;
+		int dp = pow(2, n - 1);
+		for (int i = 0; i < dp; i++)
+		{
+			midleft--;
+			midright++;
+			outchar('/', midleft);
+			outchar('\\', midright - midleft);
+			cout << endl;
+		}
+
+		outchar('A', midleft);
+		outchar('B', midright - midleft);
+		cout << endl;
+		recurPrint(midleft, n - 1);
+		recurPrint(midright, n - 1);
+	}
+}
 
 void outchar(char a, int n)
 {
@@ -102,14 +189,62 @@ void outchar(char a, int n)
 	cout << setw(setwN)<<a;
 }
 
-//在第n个位置输出红色节点
-void Routchar(char a, int n)
+void outchar(char a, int n, bool isRchildBefore)
 {
 	for (int i = 1; i<n; i++)
 	{
 		cout << setw(setwN) << ' ';
 	}
-	cout << setw(setwN - 2) << '(' << a << ')';
+	if (isRchildBefore)
+	{
+		cout << setw(setwN-1) << a;
+	}
+	else
+	{
+		cout << setw(setwN) << a;
+	}
+
+}
+
+void outchar(string a, int n)
+{
+	for (int i = 1; i<n; i++)
+	{
+		cout << setw(setwN) << ' ';
+	}
+	cout << setw(setwN) << a;
+}
+
+//在第n个位置输出红色节点
+void Routchar(char a, int n,bool isRchildBefore)
+{
+	for (int i = 1; i<n; i++)
+	{
+		cout << setw(setwN) << ' ';
+	}
+	if (isRchildBefore)
+	{
+		cout << setw(setwN - 2) << '(' << a << ')';
+	}
+	else
+	{
+		cout << setw(setwN - 1) << '(' << a << ')';
+	}
+}
+void Routchar(string a, int n, bool isRchildBefore)
+{
+	for (int i = 1; i<n; i++)
+	{
+		cout << setw(setwN) << ' ';
+	}
+	if (isRchildBefore)
+	{
+		cout << setw(setwN - 2) << '(' << a << ')';
+	}
+	else
+	{
+		cout << setw(setwN - 1) << '(' << a << ')';
+	}
 }
 
 int x[15]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
@@ -118,23 +253,59 @@ vector<int> v(15);
 int main()
 {
 	int d=log((double)15)/log(2.0)+1;
-	cout << setw(3) << ' ' << setw(3 - 2) << '(' << d << ')' << setw(3) << 'h' << endl;
-	cout << "123456789" << endl;
 	vector <char> s;
-	int tot=31;
+	int tot = 15;//tot应取3,7,15,31,63,127...
 	for (int i = 0; i < tot; i++)
-		s.push_back(char(i+'0'));
+		s.push_back(char('A'+i));
 	cout << endl << endl;
-	print2tree(s, tot);
+	printRBtree(s, tot);
+	cout << endl << endl;
 	system("pause");
 	return 1;
 }
 
+
 /**
+输出示例：
 0
+:
+.:::::::::::::::::::::::::::::::.
+:                               :
 1                               2
+:                               :
+.:::::::::::::::.               .:::::::::::::::.
+:               :               :               :
 3               4               5               6
-7       8       9       10      11      12      13      14
+:               :               :               :
+.:::::::.       .:::::::.       .:::::::.       .:::::::.
+:       :       :       :       :       :       :       :
+7       8       9       :       ;       <       =       >
+:       :       :       :       :       :       :       :
+.:::.   .:::.   .:::.   .:::.   .:::.   .:::.   .:::.   .:::.
+:   :   :   :   :   :   :   :   :   :   :   :   :   :   :   :
+?   @   A   B   C   D   E   F   G   H   I   J   K   L   M   N
+:   :   :   :   :   :   :   :   :   :   :   :   :   :   :   :
+.:. .:. .:. .:. .:. .:. .:. .:. .:. .:. .:. .:. .:. .:. .:. .:.
+: : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : :
+O P Q R S T U V W X Y Z [ \ ] ^ _ ` a b c d e f g h i j k l m n
+
+
+
+A
+:
+.:::::::::::::::::::::::.
+:                       :
+B                      (C)
+:                       :
+.:::::::::::.           .:::::::::::.
+:           :           :           :
+D          (E)          F          (G)
+:           :           :           :
+.:::::.     .:::::.     .:::::.     .:::::.
+:     :     :     :     :     :     :     :
+H    (I)    J    (K)    L    (M)    N    (O)
+
+
 
 */
 
