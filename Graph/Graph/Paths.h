54670386008942ef1b5e90739e4cc1d9afc82be6
Joe <472167879@qq.com>
2015-07-18 23:45:53 +0800

    find paths

---
+++ Graph/Graph/Paths.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "stdafx.h"
+
+class DeepFirstPaths
+{
+private:
+	vector<bool> marked;
+	vector<int> edgeTo;
+	int s;//Æðµã
+
+	void dfs(Graph &G, int v)
+	{
+		marked[v] = true;
+		vector<int>::iterator it;
+		for (it = G.getAdj(v)->begin(); it != G.getAdj(v)->end(); it++)
+		{
+			if (!marked[*it])
+			{
+				edgeTo[*it] = v;
+				dfs(G, *it);
+			}
+		}
+	}
+
+public:
+	DeepFirstPaths(Graph &G, int s)
+	{
+		this->s = s;
+		for (int i = 0; i < G.Vget(); i++)
+		{
+			marked.push_back(false);
+			edgeTo.push_back(s);
+		}
+		dfs(G, s);
+	}
+	bool hasPathTo(int v)
+	{
+		return marked[v];
+	}
+	vector<int> pathTo(int v)
+	{
+		if (!hasPathTo(v))
+			return;
+		vector<int> path;
+		for (int x = v; x != s; x = edgeTo[x])
+			path.push_back(x);
+		path.push_back(s);
+		return path;
+	}
+};
