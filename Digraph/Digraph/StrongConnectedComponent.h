f2c55afbc8fbff3174ff415dc3a98f6d3b602a09
Joe <472167879@qq.com>
2015-07-21 00:38:45 +0800

    Debug &G leave out '&'

--- Digraph/Digraph/StrongConnectedComponent.h
+++ Digraph/Digraph/StrongConnectedComponent.h
@@ -14,30 +14,33 @@ class SCC
 private:
 	vector<bool> marked;
 	vector<int> id;
-	vector<int> vtmp;
 	int count=0;
-	void dfs(Digraph G, int v)
+	void dfs(Digraph &G, int v)
 	{
 		marked[v] = true;
 		id[v] = count;
-		for (int w : *G.getAdj(v))
+		if (!(G.getAdj(v)->empty()))
 		{
-			if (!marked[v])
-				dfs(G, w);
+			for (int w : *G.getAdj(v))
+			{
+				if (!marked[w])
+					dfs(G, w);
+			}
 		}
+
 	}
 public:
-	SCC(Digraph G)
+	SCC(Digraph &G)
 	{
+		Digraph Gr = G.reverse();
+		//DepthFirstOrder ord(G.reverse());
+		DepthFirstOrder ord(Gr);
 		for (int i = 0; i < G.Vget(); i++)
 		{
 			marked.push_back(false);
 			id.push_back(0);
 		}
-		//Digraph *Gr = &G.reverse();
-		DepthFirstOrder ord(G.reverse());
-		vtmp = ord.gTopoSort();
-		for (int s : vtmp)
+		for (int s : ord.gTopoSort())
 		{
 			if (!marked[s])
 			{
@@ -45,7 +48,6 @@ public:
 				count++;
 			}
 		}
-		//Gr->~Digraph();
 	}
 	bool stronglyConnected(int v, int w)
 	{
