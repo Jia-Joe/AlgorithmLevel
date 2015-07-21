5c7e5839504fbab8707160f81bef84108da09138
Jia-Joe <472167879@qq.com>
2015-07-21 21:42:16 +0800

    MST frame

---
+++ MinSpanningTree/MinSpanningTree/MinSpanningTree.cpp
@@ -0,0 +1,13 @@
+// MinSpanningTree.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	system("pause");
+	return 0;
+}
+
---
+++ MinSpanningTree/MinSpanningTree/stdafx.h
@@ -0,0 +1,27 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <vector>
+#include <string>
+#include <list> 
+#include <string>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <tchar.h>
+
+using namespace std;
+#include "EdgeWeightedGraph.h"
+#include "PrimMST.h"
+#include "KruskalMST.h"
+
+
+
+// TODO:  在此处引用程序需要的其他头文件
