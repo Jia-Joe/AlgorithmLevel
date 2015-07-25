c241e7133ef9d97b4abfbf8221d5eeb2548771f4
Jia-Joe <472167879@qq.com>
2015-07-25 22:27:27 +0800

    Quick Find

---
+++ UnionFind/UnionFind/UF.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "stdafx.h"
+
+class UFquickfind
+{
+private:
+	vector<int> id;//第i个触点属于第id[i]个分量
+	int count;//分量总数
+public:
+	UFquickfind(int N)
+	{
+		count = N;
+		for (int i = 0; i < N; i++)
+		{
+			id.push_back(i);
+		}
+	}
+	int countGet()
+	{
+		return count;
+	}
+	bool connected(int p, int q)
+	{
+		return find(p) == find(q);
+	}
+	int find(int p)
+	{
+		return id[p];
+	}
+	void unionPQ(int p, int q)
+	{
+		int pID = find(p);
+		int qID = find(q);
+		if (pID == qID)
+			return;
+		//将p的分量重命名为q的名称
+		for (int i = 0; i < id.size(); i++)
+		{
+			if (id[i] == pID)
+				id[i] = qID;
+		}
+		count--;
+	}
+};
---
+++ UnionFind/UnionFind/UnionFind.cpp
@@ -0,0 +1,31 @@
+// UnionFind.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int N;
+	cin >> N;
+	UFquickfind uf(N);
+	while (true)
+	{
+		int p,q;
+		cin >> p >> q;
+		if (uf.connected(p, q))
+			continue;
+		uf.unionPQ(p, q);
+		if (getchar() == '\n')
+			break;
+	}
+	cout << uf.countGet()<<" comonents"<<endl;
+	for (int i = 0; i < N; i++)
+	{
+		cout << uf.find(i) << ' ';
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
+
